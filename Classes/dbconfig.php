2ddfb1f7186b85fa6c24166f5ec9dad866a28bc8
rameshkithsiri <34327045+rameshkithsiri@users.noreply.github.com>
2017-12-07 09:01:18 +0530

    Create dbconfig.php

---
+++ Classes/dbconfig.php
@@ -0,0 +1,26 @@
+<?php
+
+class databaseConfig {
+	public $connection;
+	function __construct(){
+		$host = '127.0.0.1';
+		$db   = 'catwing';
+		$user = 'root';
+		$pass = '';
+		$charset = 'utf8mb4';
+
+		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+		$opt = [
+		    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		    PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+		$this->connection = new PDO($dsn, $user, $pass, $opt);
+	}
+	
+	function sendConnection(){
+		return $this->connection;
+	}
+}
+
+?>
